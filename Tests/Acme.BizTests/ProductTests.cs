c8d9b9354bbdabad70a0d33b503868238a1fa192
Max Svetlakov <maksim.svetlakov@focisoulutions.com>
2022-02-28 16:57:08 -0500

    Add project files.

---
+++ Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+            currentProduct.Description = "15 inch steel blade saw";
+            currentProduct.ProductId = 1;
+            var expected = "Hello Saw (1): 15 inch steel blade saw";
+            //Act
+            var actual = currentProduct.SayHello();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
